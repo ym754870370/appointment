dd3a5762c34175dd7aea189b134bbaccb59c425d
yangming <wawa754870370@qq.com>
2017-11-15 11:48:36 +0800

    结构

---
+++ my-project/src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import actions from './actions.js'
+import mutations from './mutations.js'
+// import countModule from './modules/countModule.js'
+// import todosModule from './modules/todosModule.js'
+
+Vue.use(Vuex)
+
+const vuexStore = {
+	actions,
+    mutations,
+	modules: {},
+	strict: true
+}
+
+const store = new Vuex.Store(vuexStore)
+
+export default store
